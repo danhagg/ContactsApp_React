df6b158b1044015499a12e6f2f741270095a1231
danhagg <danielhaggerty1976@gmail.com>
2017-11-20 15:16:56 -0600

    form takes in and console logs info

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-// Import from MuiThemeProvider (http://www.material-ui.com/)
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import ContactForm from './Components/ContactForm';
+import ContactForm from './Components/ContactForm';
 import AppBar from 'material-ui/AppBar';
-// Import MUI themes and colors
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {teal300} from 'material-ui/styles/colors';
 
@@ -17,6 +15,7 @@ class App extends Component {
     return (
       <MuiThemeProvider muiTheme={theme}>
         <AppBar title='My Awesome Form' />
+        <ContactForm />
       </MuiThemeProvider>
     );
   }
--- contacts/src/Components/ContactForm.js
+++ contacts/src/Components/ContactForm.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
-// Add to Render:
+import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 class ContactForm extends Component {
-  render() {
+  constructor(props) {
+  super(props);
+  this.state = {name: ''};
+}
+
+update_state(event, key) {
+  console.log(event.target);
+  this.setState({[key]: event.target.value});
+}
+
+handle_submit(event) {
+  console.log('Submitted: ', this.state);
+  event.preventDefault();
+}
+
+  render () {
     return (
-      <p>From contact form<p>
+      <Card className="md-card">
+        <form onSubmit={event => this.handle_submit(event)}>
+          <CardTitle title="My Form" subtitle="subtitle"/>
+          <CardText>
+            <div>
+
+              <TextField floatingLabelText="Name"
+                defaultValue={this.state.name}
+                onChange={event => this.update_state(event, 'name')}/>
+
+              <br />
+              <TextField
+                hintText="Email"
+              /><br />
+              <TextField
+                hintText="Phone"
+              /><br />
+              <br />
+              <TextField
+                hintText="Address"
+              /><br />
+              <TextField
+                hintText="City"
+              /><br />
+              <br />
+              <TextField
+                hintText="State"
+              /><br />
+              <TextField
+                hintText="Zip"
+              /><br />
+            </div>
+          </CardText>
+          <CardActions>
+            <RaisedButton label="Submit" primary={true}/>
+          </CardActions>
+        </form>
+      </Card>
     );
   }
 }
